b058ab1b9d1899fe8cf35d4c96088ece25018755
jfcamel <fujita.jun@gmail.com>
2017-12-08 00:30:29 +0900

    [Verion 1.0] Ready to build

--- conanfile.py
+++ conanfile.py
@@ -1,30 +1,37 @@
 from conans import ConanFile, CMake
 
 
-class Stm32f4stdlibConan(ConanFile):
-    name = "stm32f4stdlib"
+class Stm32f4StdPeriphLibConan(ConanFile):
+    name = "stm32f4stdperiph"
     version = "0.1"
     license = "<Put the package license here>"
     url = "<Package recipe repository url here, for issues about the package>"
-    description = "Description of Stm32f4stdlib"
-    settings = "os", "compiler", "build_type", "arch"
+    description = "Description of libsm32f4stdperiph"
+    settings = { "os": ["Linux",],
+                 "compiler": ["gcc", ],
+                 "build_type":['Debug', 'None', 'Release'],
+                 "arch": ["armv7", "armv7hf"] }
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
-    exports_sources = "src/*"
+    exports_sources = [ "src/*", "cmake/*", "CMakeLists.txt", "*.pc" ]
+    #build_policy = "always"
 
     def build(self):
         cmake = CMake(self)
-        self.run('cmake %s/src %s' % (self.source_folder, cmake.command_line))
+        self.run('cmake %s %s' % (self.source_folder, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        self.copy("*.h", dst="include", src="src")
+        self.copy("*.h", dst="include", src="src/CMSIS/core")
+        self.copy("*.h", dst="include", src="src/CMSIS/device")
+        self.copy("*.h", dst="include", src="src/StdPeriph_Driver/inc")
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.dylib*", dst="lib", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*.pc", dst="lib/pkgconfig", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["stm32f4stdlib"]
+        self.cpp_info.libs = ["libstm32f4stdperiph"]
